ef8ac10c490ca45f7cdb2b7e660401e5e9ee6df3
h <hyeju_44@naver.com>
2025-08-22 11:40:02 +0900

    20250818

--- llm3/p6.py
+++ llm3/p6.py
@@ -1,11 +1,22 @@
-import os
-import time
-
 import streamlit as st
-
-from MyLLM import geminiTxt, geminiModel
-from PIL import Image
+from MyLLM import geminiTxt, geminiModel, save_uploadedfile
 
 st.sidebar.markdown("Clicked Page 6")
 
-st.title("Page 6")
+st.title("Page 6 File Upload")
+menu = st.selectbox("파일 타입 선택: ",["IMAGE", "PDF", "CSV"])
+
+if menu == "IMAGE":
+    st.subheader(menu)
+    file = st.file_uploader("이미지를 선택", type=["jpg","png","jpeg"])
+    if file:
+        save_uploadedfile("img", file, st)
+elif menu == "PDF":
+    st.subheader(menu)
+    file = st.file_uploader("PDF를 선택", type=["pdf"])
+    if file:
+        save_uploadedfile("pdf", file, st)
+elif menu == "CSV":
+    file = st.file_uploader("CSV를 선택", type=["csv"])
+    if file:
+        save_uploadedfile("csv", file, st)
