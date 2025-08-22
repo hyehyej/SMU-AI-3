d1021efa1785f9297aff85e7c2bc3757e65396ee
h <hyeju_44@naver.com>
2025-08-22 13:05:40 +0900

    20250818

--- llm3/p6.py
+++ llm3/p6.py
@@ -11,12 +11,30 @@ if menu == "IMAGE":
     file = st.file_uploader("이미지를 선택", type=["jpg","png","jpeg"])
     if file:
         save_uploadedfile("img", file, st)
+        st.download_button(
+            label="파일다운로드",
+            data = file,
+            file_name = file.name,
+            mime = "image/jpg"
+        )
 elif menu == "PDF":
     st.subheader(menu)
     file = st.file_uploader("PDF를 선택", type=["pdf"])
     if file:
         save_uploadedfile("pdf", file, st)
+        st.download_button(
+            label="파일다운로드",
+            data=file,
+            file_name=file.name,
+            mime="application/pdf"
+        )
 elif menu == "CSV":
     file = st.file_uploader("CSV를 선택", type=["csv"])
     if file:
         save_uploadedfile("csv", file, st)
+        st.download_button(
+            label="파일다운로드",
+            data=file,
+            file_name=file.name,
+            mime="text/text"
+        )
