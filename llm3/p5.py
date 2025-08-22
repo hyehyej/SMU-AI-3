d19a884d172b7ecd758e94c9424278a8b8f291a4
h <hyeju_44@naver.com>
2025-08-22 11:26:36 +0900

    20250818

--- llm3/p5.py
+++ llm3/p5.py
@@ -1,16 +1,34 @@
-import os
-import time
-
 import streamlit as st
-
-from MyLLM import geminiTxt, geminiModel
 from PIL import Image
+
+from MyLLM import save_carpturefile, progressBar, geminiModel
 
 st.sidebar.markdown("Clicked Page 5")
 
 st.title("Page 5")
+picture = st.camera_input("Take a picture")
+
+if picture:
+    st.info("이미지를 캡쳐했습니다")
+    save_carpturefile("capture", picture, "temp.png", st)#st 넣는이유: 이미지를 정상적으로 저장했습니다라는 메세지 출력
 
 
+    text = st.text_area(label="질문입력:",
+                        placeholder="질문을 입력 하세요")
+
+    if st.button("SEND"):
+        img = Image.open("capture/temp.png")
+        model = geminiModel()
+        my_bar = progressBar("Operation in progress. Please wait.")
+        response = model.generate_content( [ text , img] )
+        my_bar.empty()
+        st.info(response.text)
+
+
+
+
+
+'''
 def save_uploadedfile(directory, file):
     # 1. 디렉토리가 없으면 생성
     if not os.path.exists(directory):
@@ -39,3 +57,4 @@ if picture:
         model = geminiModel()
         response = model.generate_content( [ text , img ] )
         st.info(response.text)
+'''
---
+++ llm3/p6.py
@@ -0,0 +1,11 @@
+import os
+import time
+
+import streamlit as st
+
+from MyLLM import geminiTxt, geminiModel
+from PIL import Image
+
+st.sidebar.markdown("Clicked Page 6")
+
+st.title("Page 6")
